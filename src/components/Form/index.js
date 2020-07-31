52ec82a4264ad77571ffb2ad60e8f462aadef051
Nick Ray <nickrray@gmail.com>
2020-07-31 12:01:49 -0500

    :recycle: Refactor class based component to functional and use hooks

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 import { Button } from './Button'
@@ -6,74 +6,71 @@ import { Input } from './Input'
 
 import './Form.css'
 
-export class Form extends React.Component {
-  state = {
-    addStudentText: '',
-    randomStudentText: '',
-    students: []
+export const Form = (props) => {
+  const [addStudentText, setAddStudentText] = useState('')
+  const [randomStudentText, setRandomStudentText] = useState('')
+  const [students, setStudents] = useState([])
+
+  let input1 = props.inputs[0]
+  let input2 = props.inputs[1]
+  const button1 = props.buttons[0]
+  const button2 = props.buttons[1]
+
+  const handleAddStudentTextChange = (e) => {
+    setAddStudentText(e.target.value)
   }
 
-  input1 = this.props.inputs[0]
-  input2 = this.props.inputs[1]
-  button1 = this.props.buttons[0]
-  button2 = this.props.buttons[1]
-
-  handleAddStudentTextChange = (e) => {
-    this.setState({ addStudentText: e.target.value })
-  }
-
-  handleAddStudentBtnClick = (e) => {
+  const handleAddStudentBtnClick = (e) => {
     e.preventDefault()
     // Add text from input to students[] and reset input field
-    this.setState({ addStudentText: '', students: [...this.state.students, this.state.addStudentText] })
+    setAddStudentText('')
+    setStudents([...students, addStudentText])
   }
 
-  handleRandomStudentBtnClick = (e) => {
+  const handleRandomStudentBtnClick = (e) => {
     e.preventDefault()
-    if (this.state.students.length > 0) {
+    if (students.length > 0) {
       // Grab random element from state.students
-      const student = this.state.students[Math.floor(Math.random() * this.state.students.length)]
+      const student = students[Math.floor(Math.random() * students.length)]
       // Update random input with that text
-      this.setState({ randomStudentText: student })
+      setRandomStudentText(student)
       // Update input object
-      this.input2 = { ...this.input2, value: this.randomStudentText }
+      input2 = { ...input2, value: randomStudentText }
     } else {
-      this.setState({ randomStudentText: 'No students yet!' })
+      setRandomStudentText('No students yet!')
     }
   }
 
-  render() {
 
-    return (
-      <form>
-        <h1>Random Student Selector</h1>
-        <Input
-          htmlFor={this.input1.htmlFor}
-          label={this.input1.label}
-          placeholder={this.input1.placeholder}
-          handler={this.handleAddStudentTextChange}
-          value={this.state.addStudentText}
-        />
-        <Button
-          buttonText={this.button1.buttonText}
-          className={this.button1.className}
-          handler={this.handleAddStudentBtnClick}
-        />
-        <Input
-          disabled={this.input2.disabled}
-          htmlFor={this.input2.htmlFor}
-          label={this.input2.label}
-          placeholder={this.input2.placeholder}
-          value={this.state.randomStudentText}
-        />
-        <Button
-          buttonText={this.button2.buttonText}
-          className={this.button2.className}
-          handler={this.handleRandomStudentBtnClick}
-        />
-      </form>
-    )
-  }
+  return (
+    <form>
+      <h1>Random Student Selector</h1>
+      <Input
+        htmlFor={input1.htmlFor}
+        label={input1.label}
+        placeholder={input1.placeholder}
+        handler={handleAddStudentTextChange}
+        value={addStudentText}
+      />
+      <Button
+        buttonText={button1.buttonText}
+        className={button1.className}
+        handler={handleAddStudentBtnClick}
+      />
+      <Input
+        disabled={input2.disabled}
+        htmlFor={input2.htmlFor}
+        label={input2.label}
+        placeholder={input2.placeholder}
+        value={randomStudentText}
+      />
+      <Button
+        buttonText={button2.buttonText}
+        className={button2.className}
+        handler={handleRandomStudentBtnClick}
+      />
+    </form>
+  )
 }
 
 Form.propTypes = {
